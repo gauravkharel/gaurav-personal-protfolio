c9c30d74991c6f483007aa7d1350e89c6fad2968
gauravkharel <kgaurav21@tbc.edu.np>
2021-08-20 13:55:25 +0545

    initial commit for first draft

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Gaurav Personal Blog`,
+    author: `Gaurav Kharel`,
+    description: `here comes the most boring guy document things`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `gaurayyyyyy`,
     },
   },
   plugins: [
